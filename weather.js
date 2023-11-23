40880ad2745df1c573f4bbb9c582c1b52d0c96e8
thedarshanpgowda <CS115K41@ms.mcehassan.ac.in>
2023-11-23 21:01:15 +0530

    ..

--- weather.js
+++ weather.js
@@ -26,6 +26,20 @@ document.addEventListener("DOMContentLoaded", () => {
     "December",
   ];
 
+  const snowfall = `<img src="snowfall.png" alt="">`;
+  const storm = `<img src="storm.png" alt="">`;
+  const sun = `<img src="sun.png" alt="">`;
+  const tornado = `<img src="tornado.png" alt="">`;
+  const cloudy = `<img src="cloudy.png" alt="">`;
+  const fully_cloud = `<img src="fully_cloud.png" alt="">`;
+  const night = `<img src="night.png" alt="">`;
+  const overcast = `<img src="overcast.png" alt="">`;
+  const sleet = `<img src="sleet.png" alt="">`;
+  const Partlycloudy = `<img src="Partlycloudy.png" alt="">`;
+  const rainy = `<img src="rainy.png" alt="">`;
+  const partlyrain = `<img src="partlyrain.png" alt="">`;
+  const windy = `<img src="windy.png" alt="">`;
+
   const weather = async (city) => {
     let response = await fetch(url + city + apiKey);
     let data = await response.json();
@@ -34,13 +48,41 @@ document.addEventListener("DOMContentLoaded", () => {
     let k = data.main.temp.toFixed(1);
     let date = new Date();
     document.querySelector("#Actualtemperature").innerHTML = `${k}&deg;C `;
-    document.querySelector("#humid").innerHTML = `${data.main.humidity}&deg;C `;
+    document.querySelector("#humid").innerHTML = `${data.main.humidity}% `;
+    document.querySelector(
+      "#message"
+    ).innerHTML = `${data.weather[0].description} `;
+    document.querySelector(
+      "#Cloudmessage"
+    ).innerHTML = `${data.weather[0].description} `;
     document.querySelector("#wind").innerHTML = `${data.wind.speed}km/h `;
     document.querySelector("#place").innerHTML = `${data.name} `;
     document.querySelector("#day").innerHTML = `${day[date.getDay()]}`;
     document.querySelector("#date").innerHTML = `${
       months[date.getMonth()]
     }, ${date.getFullYear()}`;
+    const pics = document.querySelector(".blockforImaage");
+    if (k > 20) {
+      pics.innerHTML = `${Partlycloudy}`;
+    }
+    if (k > 35) {
+      pics.innerHTML = `${sun}`;
+    }
+    if (k < 20 && data.main.humidity < 70) {
+      pics.innerHTML = `${fully_cloud}`;
+    }
+    if (data.main.humidity > 70) {
+      pics.innerHTML = `${overcast}`;
+    }
+    if (data.main.temp < 1) {
+      pics.innerHTML = `${sleet}`;
+    }
+    if (date.getHours() < 5 && date.getHours() > 21) {
+      pics.innerHTML = `${night}`;
+    }
+    if (k <= 0 && data.main.humidity >= 80 && data.wind.speed < 5){
+      pics.innerHTML = `${snowfall}`;
+    }
   };
   var city;
 
@@ -49,9 +91,16 @@ document.addEventListener("DOMContentLoaded", () => {
     weather(city);
   });
   const input = document.getElementById("cityName");
+
   input.addEventListener("keypress", (e) => {
-    // console.log(e.key);
     if (e.code === "Enter") {
+      e.preventDefault();
+      city = document.getElementById("cityName").value;
+      weather(city);
+    }d
+  });
+  input.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" || e.code === "Enter") {
       e.preventDefault();
       city = document.getElementById("cityName").value;
       weather(city);
